55712a6a014bca3270568272148e4c70d232db0e
Ivan Vaniuk <ivanvanuk77@gmail.com>
2025-05-02 14:48:43 +0300

    make build

---
+++ index.fed9184a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=Math.round(e.clientHeight/2-t.offsetHeight/2),o=Math.round(e.clientWidth/2-t.offsetWidth/2);t.style.left=o+"px",t.style.top=l+"px";
+//# sourceMappingURL=index.fed9184a.js.map
